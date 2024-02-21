7fb4d79427fb6856538b92751af7032621407caa
Fabrice-Perso <magnan.fabrice@gmail.com>
2024-02-21 11:48:08 +0100

    correction faite

--- src/containers/Form/index.js
+++ src/containers/Form/index.js
@@ -10,11 +10,12 @@ const Form = ({ onSuccess, onError }) => {
   const [sending, setSending] = useState(false);
   const sendContact = useCallback(
     async (evt) => {
-      evt.preventDefault();
+      evt.preventDefault();      
       setSending(true);
-      // We try to call mockContactApi
+      // Nous essayons d'appeler mockContactApi
       try {
         await mockContactApi();
+        onSuccess();
         setSending(false);
       } catch (err) {
         setSending(false);
@@ -36,7 +37,8 @@ const Form = ({ onSuccess, onError }) => {
             type="large"
             titleEmpty
           />
-          <Field placeholder="" label="Email" />
+          <Field placeholder="" label="Email" 
+          />
           <Button type={BUTTON_TYPES.SUBMIT} disabled={sending}>
             {sending ? "En cours" : "Envoyer"}
           </Button>
